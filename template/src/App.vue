2249944a5a585559533b939a7adffde6ccb0f99c
cmprat <cmprat@minsait.com>
2018-07-11 11:34:35 +0200

    added Onesait lateral navigation

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="app">
+    {{#if_eq onesait "yes"}}
+    <ods-container>
+      <ods-aside width="72px">
+        <side-navigation></side-navigation>
+      </ods-aside>
+      <ods-container>
+        <ods-header>Cabecera</ods-header>
+        <ods-main>
+          {{#router}}
+          <router-view/>
+          {{else}}
+          <HelloWorld/>
+          {{/router}}
+        </ods-main>
+      </ods-container>
+    </ods-container>
+    {{/if_eq}}
+    {{#if_eq onesait "noOds"}}
     <img src="./assets/images/logo.png">
     {{#router}}
     <router-view/>
     {{else}}
     <HelloWorld/>
     {{/router}}
+    {{/if_eq}}
   </div>
 </template>
 
 <script>
 {{#unless router}}
 import HelloWorld from './views/HelloWorld'
+import SideNavigation from './components/SideNavigation'
 
 {{/unless}}
 export default {
   name: 'App'{{#router}}{{else}},
   components: {
-    HelloIndra
+    HelloIndra,
+    SideNavigation
   }{{/router}}
 }
 </script>
@@ -28,8 +49,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-kerning: none;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
 }
 </style>
