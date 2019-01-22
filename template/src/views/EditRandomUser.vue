2fb0d1f1788d334ad323d5b76a6b6381332715c4
beltranrengifo <beltran@rengifo.es>
2019-01-22 16:30:05 +0100

    FIX escaped template vars

--- template/src/views/EditRandomUser.vue
+++ template/src/views/EditRandomUser.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div
-  h1 Editing random user: {{ $route.params.id }}
+  h1 Editing random user: \{{ $route.params.id }}
   ods-button.ods-mt-12(@click="confirmation") Confirmation
 </template>
 
