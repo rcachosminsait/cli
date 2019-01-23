c07752fc98bf04cc4931dcb14e8ced2a408ff534
beltranrengifo <beltran@rengifo.es>
2019-01-23 10:20:59 +0100

    FIX params example

--- template/src/views/RandomUser.vue
+++ template/src/views/RandomUser.vue
@@ -1,10 +1,24 @@
 <template lang="pug">
 div
-  h1 Random user: \{{ $route.params.id }}
+  h1.ods-mb-12 Random user: \{{ $route.params.id }}
+  ods-button(@click="editRandomUser") Edit random User
 </template>
 
 <script>
 export default {
-  name: 'RandomUser'
+  name: 'RandomUser',
+  methods: {
+    editRandomUser () {
+      let num = this.generateId(0, 200)
+      let str = Math.random().toString(36).substring(7)
+      sessionStorage.setItem('randomStr', `random Str ${str}`)
+      this.$router.push(`/users/random-user/${num}/edit/`)
+    },
+    generateId (min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+  }
 }
 </script>
--- template/src/views/Users.vue
+++ template/src/views/Users.vue
@@ -2,8 +2,6 @@
 div
   h1.ods-mb-12 Users
   ods-button(@click="getRandomUser") Get random User
-  ods-button(@click="editRandomUser") Edit random User
-  router-view
 </template>
 
 <script>
@@ -14,11 +12,6 @@ export default {
       let num = this.generateId(0, 200)
       sessionStorage.setItem('userInfo', `SessionStorage ${num}`)
       this.$router.push(`/users/random-user/${num}`)
-    },
-    editRandomUser () {
-      let str = Math.random().toString(36).substring(7)
-      sessionStorage.setItem('randomStr', `random Str ${str}`)
-      this.$router.push(`/users/random-user/${this.generateId(0, 200)}/edit/`)
     },
     generateId (min, max) {
       min = Math.ceil(min)
